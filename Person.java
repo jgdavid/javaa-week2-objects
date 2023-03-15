c973cac158e228ac7d2c29aad377aace1be43f81
jgdavid <jdavid@qa.com>
2023-03-15 13:12:17 +0000

    init

---
+++ Person.java
@@ -0,0 +1,14 @@
+package java_week2_objects;
+
+public class Person {
+	String name;
+	int age;
+	String job;
+
+	public Person(String name, int age, String job) {
+
+		this.name = name;
+		this.age = age;
+		this.job = job;
+	}
+}
---
+++ object_exercise.java
@@ -0,0 +1,15 @@
+package java_week2_objects;
+
+public class object_exercise {
+	public static void main(String[] args) {
+		
+		Person p1 = new Person("Tom", 26, "Mouse");
+//		p1.name = "Tom";
+//		p1.age = 26;
+//		p1.job = "Mouse";
+		
+		Person p2 = new Person("Jerry", 34 "Cat");
+		
+		Person p3 = new Person("Ben", 68, "Flower pot guy");
+	}
+}
